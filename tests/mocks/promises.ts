9caed55516c7fbbf4159b8dad4f5ecf6bfc7f2a3
Kirill Romanov <djaler1@gmail.com>
2022-04-23 23:40:10 +0300

    chore: setup monorepo with separate package for tests

--- tests/mocks/promises.ts
+++
@@ -1,7 +0,0 @@
-import { setImmediate } from 'timers';
-
-export function flushPromises(): Promise<void> {
-    return new Promise((resolve) => {
-        setImmediate(resolve);
-    });
-}
--- tests/setup.ts
+++
@@ -1,6 +0,0 @@
-import VueCompositionApi from '@vue/composition-api';
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-
-Vue.use(VueCompositionApi);
-Vue.use(VueRouter);
