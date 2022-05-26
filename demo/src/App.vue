a19263e1fde51589bd817fe6abf1a8bedc9af357
Kirill Romanov <djaler1@gmail.com>
2022-05-26 21:46:31 +0300

    docs: demo on github pages

---
+++ demo/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <main class="container">
+        <router-view/>
+    </main>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'App',
+    });
+</script>
