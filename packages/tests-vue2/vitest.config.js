20e861c3479d4ad38fef88eea3531d5e8c49bd14
Kirill Romanov <djaler1@gmail.com>
2022-07-31 22:10:45 +0300

    test(deps): update vitest to 0.20.2
    Signed-off-by: Kirill Romanov <djaler1@gmail.com>

--- packages/tests-vue2/vitest.config.js
+++ packages/tests-vue2/vitest.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@vue/composition-api': '@vue/composition-api/dist/vue-composition-api.common.js',
-        },
-    },
     test: {
         environment: 'jsdom',
     },
